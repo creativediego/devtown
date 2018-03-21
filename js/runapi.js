15797f969d543e9f0ed697a55e747f5cb79efe58
Hnin Haymar <hninhaymar@gmail.com>
2018-03-20 20:53:09 -0700

    added makeChart skeleton function for CityScoresAPI

--- js/runapi.js
+++ js/runapi.js
@@ -21,28 +21,36 @@ var RunApi = Class.create({ //abstract parent class
             onFailure : function() { console.log("something went wrong");}
         });
         //make an ajax call with this.url and this.searchTerm
+    },
+    makeChart : function() {
+        //virtual function, implement it in child classes
     }
 });
 
 var CityScoresAPI = Class.create(RunApi,{
     initialize : function($super,searchTerm){
-        $super(searchTerm);
-        
+        $super(searchTerm);     
         this.url = "https://api.teleport.org/api/urban_areas/slug:"+this.searchTerm+"/scores/";
-
-        this.run();
     },
     processData : function($super, data){
         console.log("Running ProcessData in CityScores API");
         
         var scores = data.responseJSON.categories;
         var obj = {};
+        var labels = [];
+        var chartData = [];
         scores.each(function(element){
             obj[element.name] = Math.round(element.score_out_of_10);
+            labels.push(element.name);
+            chartData.push(element.score_out_of_10);
+            
         });
-
         console.log(obj);
+        makeChart(labels,chartData);
         return obj;
+    },
+    makeChart : function(labels,chartData){
+        //make chart object and update the chart div on html
     }
 });
 
@@ -50,7 +58,6 @@ var SalariesAPI = Class.create(RunApi,{
     initialize : function($super,searchTerm){
         $super(searchTerm);
         this.url = "https://api.teleport.org/api/urban_areas/slug:"+this.searchTerm+"/salaries/";
-        this.run();
     },
     processData : function($super, data){
         //$super(data);
@@ -67,7 +74,6 @@ var SalariesAPI = Class.create(RunApi,{
             }
         });
 
-        console.log(obj);
         return obj;
     }
 });
@@ -79,8 +85,6 @@ var JobsAPI = Class.create(RunApi,{
         this.description = "description=web+development" + "&";
         this.searchTerm = "location="+this.searchTerm+"&";
         this.url += this.description + this.searchTerm;
-
-        this.run();
     },
     processData : function(data){
         console.log("Running ProcessData in JobsAPI")
