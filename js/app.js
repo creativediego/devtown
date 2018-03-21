1c440109b0ac3cae9763f8f500f1c5eb59c63222
Hnin Haymar <hninhaymar@gmail.com>
2018-03-20 19:25:14 -0700

    Implemented CityScoresAPI and SalariesAPI and processData function for them

--- js/app.js
+++
@@ -1,3 +0,0 @@
-var cityAPI = new CityScoresAPI("https://api.teleport.org/api/urban_areas/slug:san-diego/scores/");
-
-cityAPI.run();
--- js/runapi.js
+++ js/runapi.js
@@ -3,13 +3,14 @@ var RunApi = Class.create({
     initialize : function(url,searchTerm) {
         this.url = url;
         this.searchTerm = searchTerm;
+
+        this.run();
     },
     
     processData : function(data){
         console.log("ProcessData function");
-        console.log(data.responseJSON);
         //this function will need to be overwritten by child classes
-        //because we would be handling different API's differently
+        //because we would be handling data from different API's differently
     },
     run : function(){
         console.log("run function");
@@ -31,8 +32,38 @@ var RunApi = Class.create({
 
 var CityScoresAPI = Class.create(RunApi,{
     processData : function($super, data){
-        $super(data);
         console.log("Running ProcessData in CityScores API");
+        
+        var scores = data.responseJSON.categories;
+        var obj = {};
+        scores.each(function(element){
+            obj[element.name] = element.score_out_of_10;
+        });
+
+        console.log(obj);
+        return obj;
+    }
+});
+
+var SalariesAPI = Class.create(RunApi,{
+    processData : function($super, data){
+        //$super(data);
+        console.log("Running ProcessData in Salaries API");
+        
+        var salaries = data.responseJSON.salaries;
+        console.log(salaries);
+        var obj = {};
+        //get "web developer" salary
+        salaries.each(function(element){
+            if(element.job.title === "Web Developer"){
+                obj["pct_25"] = Math.round(element.salary_percentiles.percentile_25).toLocaleString();
+                obj["pct_50"] = Math.round(element.salary_percentiles.percentile_50).toLocaleString();
+                obj["pct_75"] = Math.round(element.salary_percentiles.percentile_75).toLocaleString();
+            }
+        });
+
+        console.log(obj);
+        return obj;
     }
 });
 
