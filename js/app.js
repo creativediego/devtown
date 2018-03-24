b59033e642a75e9e4f2cc30638c241de3ac8ecd7
Developer <developer@Creatives-MacBook-Pro.local>
2018-03-24 00:37:08 -0700

    revised front end and lifestyle data

--- js/app.js
+++ js/app.js
@@ -7,34 +7,65 @@ var obj = coordinates.run();*/
 //var salariesAPI = new SalariesAPI("San Diego");
 
 //var jobs = new JobsAPI("new york");).
+j$(document).ready(function() {
 
 
-j$("#city-submit").on("click",function(){
-    event.preventDefault();
-    console.log("city submit");
+    //Search Submit
+    j$("#city-submit, #city-submit-nav").on("click", function(event) {
+        event.preventDefault();
+        console.log("city submit");
 
-    var input_city = j$("#input-city").val().trim();
-    console.log(input_city);
+        //Empty current info
+        j$("#lifestyle").empty();
 
-    //validate input_city
-    var cityAPI = new CityScoresAPI(input_city);
-    cityAPI.run();
+        //Change input city based on which search button was clicked
+        if (j$(this).is("#city-submit")) {
+            var input_city = j$("#city-input").val().trim();
+            console.log(input_city);
+        } else if (j$(this).is("#city-submit-nav"))
+            var input_city = j$("#city-input-nav").val().trim();
+        console.log(input_city); {
 
-    var coordinates = new GetCoordinates(input_city);
-    var obj = coordinates.run();
-    
-});
-
-
-var searchOptions = {
-    url: "js/cities.json",
-    getValue: "name",
-    list: {
-        match: {
-            enabled: true
         }
-    }
-};
+
+        //validate input_city
+        var cityAPI = new CityScoresAPI(input_city);
+        cityAPI.run();
+
+        var coordinates = new GetCoordinates(input_city);
+        var obj = coordinates.run();
+
+        //Display nav search
+        j$("#nav-search").css("display", "flex")
+
+    });
+
+    //Easy Auto Complete
+    var searchOptions = {
+        url: "js/cities.json",
+        getValue: "name",
+        list: {
+            match: {
+                enabled: true
+            }
+        }
+    };
+
+    j$("#city-input").easyAutocomplete(searchOptions);
+    j$("#city-input-nav").easyAutocomplete(searchOptions);
+    j$(".easy-autocomplete").css("width", "100%");
+
+    //Shuffle search background pictures
+    var imageList = ["sandiegoskyline", "boston_1", "boston_2", "central-park-meadow-nyc", "ny-city", "seattle"]
+    var image = imageList[Math.floor(Math.random() * imageList.length)];
+    j$("#search-jumbotron").css("background-image", "url(images/" + image + ".jpg)");
 
 
-j$("#input-city").easyAutocomplete(searchOptions);
+    //Data Buttons
+    j$("body").on("click", "#lifestyle-data-button", function() {
+
+        j$("#lifestyle-scores").css("display", "flex");
+    })
+
+
+});
